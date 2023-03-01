358618c12deb0c39fb3efc054996ef79689dc995
nofurtherinformation <dhalpern@uchicago.edu>
2023-02-28 22:17:15 -0600

    Tooltip cleanup

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,43 +1,51 @@
 import { create } from "zustand";
 import { TooltipSchema } from "@/config/TooltipColumns";
-import type {PickingInfo } from '@deck.gl/core/typed';
+import type { PickingInfo } from "@deck.gl/core/typed";
 
 export const useTooltipStore = create<{
   tooltipInfo: any;
   setTooltipInfo: (tooltipInfo: PickingInfo) => void;
 }>((set) => ({
   tooltipInfo: {},
-  setTooltipInfo: (tooltipInfo: PickingInfo) => set((state) => ({ tooltipInfo })),
+  setTooltipInfo: (tooltipInfo: PickingInfo) =>
+    set((state) => ({ tooltipInfo })),
 }));
 
-const NAV_OFFSET = 84
+const NAV_OFFSET = 84;
 
 export function Tooltip({ columns }: { columns: Array<TooltipSchema> }) {
   const tooltipInfo = useTooltipStore((state) => state.tooltipInfo);
   if (typeof window === "undefined") return null;
-  const {x,y,object} = tooltipInfo
-  const directionX = x < window.innerWidth / 2 ? 'left' : 'right'
-  const directionY = y < window.innerHeight / 2 ? 'top' : 'bottom'
+  const { x, y, object } = tooltipInfo;
+  const directionX = x < window.innerWidth / 2 ? "left" : "right";
+  const directionY = y < window.innerHeight / 2 ? "top" : "bottom";
   const [posX, posY] = [
-    directionX === 'left' ? x + 10 : window.innerWidth - x + 10,
-    directionY === 'top' ? y + 10 + 84: window.innerHeight - y + 20 - 84
-  ]
-  const properties = object?.properties
-  if (x === undefined || x===-1 || !properties) return null
+    directionX === "left" ? x + 10 : window.innerWidth - x + 10,
+    directionY === "top" ? y + 10 + 84 : window.innerHeight - y + 20 - 84,
+  ];
+  const properties = object?.properties;
+  if (x === undefined || x === -1 || !properties) return null;
 
   return (
-    <div className="fixed opacity-75 text-left" style={{
-      [directionX]: posX,
-      [directionY]: posY
-    }}>
+    <div
+      className="fixed opacity-75 text-left"
+      style={{
+        [directionX]: posX,
+        [directionY]: posY,
+      }}
+    >
       <table className="table table-zebra table-compact">
         <tbody>
-          {columns.map(({column, label, format}) => (
-            <tr key={label}>
-              <td>{label}:</td>
-              <td>{format ? format(properties[column]) : properties[column]}</td>
-            </tr>
-          ))}
+          {columns.map(({ column, label, format }) =>
+            column in properties ? (
+              <tr key={label}>
+                <td>{label}:</td>
+                <td>
+                  {format ? format(properties[column]) : properties[column]}
+                </td>
+              </tr>
+            ) : null
+          )}
         </tbody>
       </table>
       {/* {columns.map(({column, label}) => (
