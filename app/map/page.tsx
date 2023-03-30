89b83ba09319a12b4b71ab7cdd28c2f49bebb1d4
nofurtherinformation <dhalpern@uchicago.edu>
2023-03-30 09:39:32 -0500

    Split Tooltips by scale

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -30,7 +30,10 @@ import { Deck } from "@deck.gl/core/typed";
 import maplibre from "maplibre-gl";
 import { mapVariables } from "@/config/MapVariables";
 import { Tooltip, useTooltipStore } from "@/components/Tooltip";
-import { tooltipColumns } from "@/config/TooltipColumns";
+import {
+  blockTooltipColumns,
+  regionTooltipColumns,
+} from "@/config/TooltipColumns";
 import type { PickingInfo } from "@deck.gl/core/typed";
 import { choroplethOpacity } from "@/config/MapSettings";
 import GeocoderControl from "@/components/Geocoder";
@@ -67,107 +70,17 @@ const loadOptions = {
   },
 };
 
-const colors = [
-  "#E8E7F4",
-  "#CEC8DA",
-  "#93328E",
-  "#B83A85",
-  "#D9477A",
-  "#F25D6D",
-  "#FB7962",
-  "#FF9859",
-  "#FFB857",
-  "#FCD860",
-  "#F9F871",
-];
-
-function toRgb(c: string) {
-  if (c.length != 6) {
-    throw "Only six-digit hex colors are allowed.";
-  }
-
-  var aRgbHex = c.match(/.{1,2}/g);
-  var aRgb = [
-    // @ts-ignore
-    parseInt(aRgbHex[0], 16),
-    // @ts-ignore
-    parseInt(aRgbHex[1], 16),
-    // @ts-ignore
-    parseInt(aRgbHex[2], 16),
-  ];
-  return aRgb;
-}
-
-const kcomplexityColorScheme = [
-  {
-    value: 1,
-    color: toRgb("E8E7F4"),
-    label: "1",
-  },
-  {
-    value: 2,
-    color: toRgb("CEC8DA"),
-    label: "2",
-  },
-  {
-    value: 3,
-    color: toRgb("93328E"),
-    label: "3",
-  },
-  {
-    value: 4,
-    color: toRgb("B83A85"),
-    label: "4",
-  },
-  {
-    value: 5,
-    color: toRgb("D9477A"),
-    label: "5",
-  },
-  {
-    value: 6,
-    color: toRgb("F25D6D"),
-    label: "6",
-  },
-  {
-    value: 7,
-    color: toRgb("FB7962"),
-    label: "7",
-  },
-  {
-    value: 8,
-    color: toRgb("FF9859"),
-    label: "8",
-  },
-  {
-    value: 9,
-    color: toRgb("FFB857"),
-    label: "9",
-  },
-  {
-    value: 10,
-    color: toRgb("FCD860"),
-    label: "10+",
-  },
-  // {
-  //   value: 999,
-  //   color: toRgb("F9F871"),
-  //   label: "Off Network+",
-  // },
-];
-
 export default function App() {
   const [variable, setVariable] = useState(mapVariables[0].name);
   const [tileContent, setTileContent] = useState({});
   const [z, setZ] = useState(INITIAL_VIEW_STATE.zoom);
   const [showSatellite, setShowSatellite] = useState(false);
   const [layersLoaded, setLayersLoaded] = useState({});
-  // const zTransitionLevel = 7;
-  // const numZfade = 4;
-
-  // const colorScale = "warm";
+  const [hoveredLayer, setHoveredLayer] = useState("");
+  const tooltipColumns = hoveredLayer.includes("blocks")
+    ? blockTooltipColumns
+    : regionTooltipColumns;
   const setTooltipInfo = useTooltipStore((state) => state.setTooltipInfo);
-  // @ts-ignore
   const currSchema = useMemo(
     () => mapVariables.find((v) => v.name === variable) || mapVariables[0],
     [variable]
@@ -219,7 +132,13 @@ export default function App() {
   );
 
   const handleTooltipInfo = (info: PickingInfo) => {
-    info && setTooltipInfo(info);
+    if (!info) {
+      setTooltipInfo({} as PickingInfo);
+      return;
+    }
+    const layer = info?.layer?.id;
+    layer && setHoveredLayer(layer);
+    setTooltipInfo(info);
   };
 
   const setLayerLoaded = (layer: string) => {
--- config/TooltipColumns.ts
+++ config/TooltipColumns.ts
@@ -7,7 +7,7 @@ export type TooltipSchema = {
   format?: (value: any) => string;
 }
 
-export const tooltipColumns: Array<TooltipSchema> = [
+export const regionTooltipColumns: Array<TooltipSchema> = [
   {
     column: "country_name",
     label: "Country",
@@ -74,3 +74,70 @@ export const tooltipColumns: Array<TooltipSchema> = [
   },
 ]
 
+
+export const blockTooltipColumns: Array<TooltipSchema> = [
+  {
+    column: "country_name",
+    label: "Country",
+  },
+  {
+    column: "agglosname",
+    label: "Place name",
+  },
+  {
+    column: "area_type",
+    label: "Block type",
+  },
+  {
+    column: "k_ls_labels",
+    label: "Block complexity",
+  },
+  {
+    column: "k_labels_detailed",
+    label: "Block complexity",
+  },
+  {
+    column: (properties) => ({wp: properties["worldpop_population_un"], ls: properties["landscan_population_un"]}),
+    label:"Population Estimate km²",
+    format: (data: {wp:number,ls:number}) => `${Math.round(data.ls).toLocaleString()} (LS) | ${Math.round(data.wp).toLocaleString()} (WP)`
+  },
+  {
+    column: "worldpop_population_un",
+    label: "Population estimate (WorldPop)",
+    format: (v:number) => Math.round(v).toLocaleString()
+  },
+  {
+    column: "landscan_population_un_density_hectare",
+    label: "Population per hectare (LandScan)",
+    format: (v:number) => (Math.round(v*100)/100).toLocaleString()
+  },
+  {
+    column: "worldpop_population_un_density_hectare",
+    label: "Population per hectare (WorldPop)",
+    format: (v:number) => (Math.round(v*100)/100).toLocaleString()
+  },
+  {
+    column: "building_count",
+    label: "Number of buildings",
+    format: (v:number) => Math.round(v).toLocaleString()
+  },
+  {
+    column: "block_hectares",
+    label: "Block area",
+    format: (v:number) => `${(Math.round(v*100)/100).toLocaleString()} (hectares)`
+  },
+  {
+    column: "block_area_km2",
+    label: "Block area",
+    format: (v:number) => `${(Math.round(v*100)/100).toLocaleString()}  (hectares)`
+  },
+  {
+    column: "average_building_area_m2",
+    label: "Average building area (meters square)",
+    format: (v:number) => (Math.round(v*100)/100).toLocaleString()
+  },
+  {
+    column: "block_id",
+    label: "Block ID",
+  },
+]
